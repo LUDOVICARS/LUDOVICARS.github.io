11b17e4223ad3fc5cc37961660f51cf143fae04e
LUDOVICARS <jeanjacques2laretraite@gmail.com>
2017-03-10 15:01:32 +0100

    Update bitcoin.js

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -35,3 +35,22 @@ $(document).ready(function() {
 		}
 	});
 });
+
+$(document).ready(function() {
+	$.ajax({
+		url : "https://api.blockchain.info/stats?cors=true",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_blocks').append(data.market_price_usd);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_blocks').append(err+" N/A");
+		}
+	});
+});
