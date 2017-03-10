8eb124fac1122ef28708e590a1e3c7dfc9bed85f
LUDOVICARS <jeanjacques2laretraite@gmail.com>
2017-03-10 15:48:57 +0100

    Update bitcoin.js

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -28,48 +28,15 @@ $(document).ready(function() {
 
 		success : function(data) {
 			$('#bitcoin_network_hash').append(data.minutes_between_blocks);
-		},
-
-		error : function(xhr, status, err) {
-			$('#bitcoin_network_hash').append(err+" N/A");
-		}
-	});
-});
-
-$(document).ready(function() {
-	$.ajax({
-		url : "https://api.blockchain.info/stats?cors=true",
-		dataType : "json",
-		contentType : "application/json; charset=utf-8",
-		type : "GET",
-		timeout:	"5000",
-		async : false,
-
-		success : function(data) {
 			$('#bitcoin_usd_price').append(data.market_price_usd);
-		},
-
-		error : function(xhr, status, err) {
-			$('#bitcoin_usd_price').append(err+" N/A");
-		}
-	});
-});
-
-$(document).ready(function() {
-	$.ajax({
-		url : "https://api.blockchain.info/stats?cors=true",
-		dataType : "json",
-		contentType : "application/json; charset=utf-8",
-		type : "GET",
-		timeout:	"5000",
-		async : false,
-
-		success : function(data) {
 			$('#bitcoin_blocks').append(data.estimated_transaction_volume_usd);
 		},
 
 		error : function(xhr, status, err) {
+			$('#bitcoin_network_hash').append(err+" N/A");
+			$('#bitcoin_usd_price').append(err+" N/A");
 			$('#bitcoin_blocks').append(err+" N/A");
 		}
 	});
 });
+
